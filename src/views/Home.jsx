932ca0ee1f2cd803771b8d91b5705d880a37512b
Manuel Corao <scorao16@gmail.com>
2023-11-01 23:56:37 -0500

    :camera_flash: Desafio Natural Pic

---
+++ src/views/Home.jsx
@@ -0,0 +1,11 @@
+import Gallery from "../components/Gallery";
+
+const Home = () => {
+    return (
+        <div className="App">
+            <h1 className="Titulo">Natural Pic</h1>
+            <Gallery />
+        </div>
+    );
+};
+export default Home;
